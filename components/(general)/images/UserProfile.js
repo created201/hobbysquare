f8aefb089ad3c1a37457d3f9ab6349909461447a
Jin Lee <yjzlee@ucdavis.edu>
2023-05-20 16:56:03 -0700

    added: navbar animation; fixed: footer layout for desktop

--- components/(general)/images/UserProfile.js
+++ components/(general)/images/UserProfile.js
@@ -7,8 +7,8 @@ const UserProfile = ({ src }) => {
             alt="User Pic"
             className={
                 src
-                    ? "rounded-full w-10 h-10 border-solid border-2"
-                    : "rounded-full w-10 h-10 border-solid border-2 bg-emerald-100"
+                    ? "rounded-full w-7 h-7 xl:w-10 xl:h-10 border-solid border-2"
+                    : "rounded-full w-7 h-7 xl:w-10 xl:h-10 border-solid border-2 bg-emerald-100"
             }
         />
     )
--- components/(general)/navs/DesktopNav.js
+++ components/(general)/navs/DesktopNav.js
@@ -1,12 +1,10 @@
 import dynamic from "next/dynamic"
-import UserProfile from "../images/UserProfile"
 
 const NavButton = dynamic(() => import("@/(general)/buttons/NavButton"))
 
 const DesktopNav = () => {
     return (
         <nav className="lg:flex hidden items-center gap-x-4">
-            <UserProfile src="" />
             <NavButton user="jin lee" />
         </nav>
     )
