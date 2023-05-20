3f8ad5c5ec937c8bd7ec5eddde7fb603cd3b19c3
yetigang404 <105612960+yetigang404@users.noreply.github.com>
2023-05-20 14:08:48 -0700

    added: login page

---
+++ pages/login.js
@@ -0,0 +1,19 @@
+import dynamic from "next/dynamic"
+const PrimaryLayout = dynamic(() => import("@/(general)/layouts/PrimaryLayout"))
+
+const LoginPage = () => {
+    return (
+        // Index Page Content
+        <div className="h-screen flex flex-col justify-center items-center">
+            <section className="px-8 2xl:px-0 max-w-cutoff mx-auto w-full">
+                LoginPage
+            </section>
+        </div>
+    )
+}
+
+LoginPage.getLayout = (page) => {
+    return <PrimaryLayout>{page}</PrimaryLayout>
+}
+
+export default LoginPage
