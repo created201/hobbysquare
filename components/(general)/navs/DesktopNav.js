531e21fbbdca1c10a0be68fea5f0bc2cb81faa6f
Jin Lee <yjzlee@ucdavis.edu>
2023-05-20 16:59:26 -0700

    fixed: user profile shows in desktop screen

--- components/(general)/navs/DesktopNav.js
+++ components/(general)/navs/DesktopNav.js
@@ -1,10 +1,12 @@
 import dynamic from "next/dynamic"
+import UserProfile from "../images/UserProfile"
 
 const NavButton = dynamic(() => import("@/(general)/buttons/NavButton"))
 
 const DesktopNav = () => {
     return (
         <nav className="lg:flex hidden items-center gap-x-4">
+            <UserProfile />
             <NavButton user="jin lee" />
         </nav>
     )
